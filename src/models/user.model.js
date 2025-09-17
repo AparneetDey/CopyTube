5ff592dd808ac70ef793a52fbd173b54cbaaa5e5
Your Name <aparnitstudy@gmail.com>
2025-09-17 22:17:00 +0530

    Created user and video data model

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+        },
+        avatar: {
+            type: String, //CLoudinary URL
+            required: true,
+        },
+        coverImage: {
+            type: String, //CLoudinary URL
+            required: true,
+        },
+        watchHistory: [
+            {  
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema} from "mongoose";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //CLoudinary URL
+            required: true,
+        },
+        thumbnail: {
+            type: String, //CLoudinary URL
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            required: true,
+        },
+        isPublished: {
+            type: Boolean,
+            Default: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+
+export const Video = mongoose.model("Video", videoSchema);
