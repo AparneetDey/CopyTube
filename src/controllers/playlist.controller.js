52601d5a322f6229f7a6c18ef81d9033e6b183ac
Your Name <aparnitstudy@gmail.com>
2025-10-04 23:36:39 +0530

    Add video management functionality to playlists. Implement addVideoToPlaylist and removeVideoFromPlaylist controllers. Update playlist routes to include new endpoints for adding and removing videos from playlists. Ensure error handling for missing IDs. Tested and Debugged.

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -4,10 +4,10 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { Playlist } from "../models/playlist.model.js";
 
 
-const createPlayList = asyncHandler( async (req, res) => {
+const createPlayList = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
 
-    if(!name) throw new ApiError(400, "Name is Required");
+    if (!name) throw new ApiError(400, "Name is Required");
 
     const createdPlayList = await Playlist.create({
         name,
@@ -15,68 +15,124 @@ const createPlayList = asyncHandler( async (req, res) => {
         owner: req.user?._id
     });
 
-    if(!createPlayList) throw new ApiError(500, "Something went wrong while creating the playlist");
+    if (!createPlayList) throw new ApiError(500, "Something went wrong while creating the playlist");
 
     res
-    .status(201)
-    .json(
-        new ApiResponse(
-            200,
-            createdPlayList,
-            "Playlist Created Successfully"
+        .status(201)
+        .json(
+            new ApiResponse(
+                200,
+                createdPlayList,
+                "Playlist Created Successfully"
+            )
         )
-    )
 })
 
-const getUserPlayLists = asyncHandler( async (req, res) => {
+const getUserPlayLists = asyncHandler(async (req, res) => {
     const { userId } = req.params;
 
-    if(!userId) throw new ApiError(400, "User Id is Required");
+    if (!userId) throw new ApiError(400, "User Id is Required");
 
     const userPlayList = await Playlist.find({
         owner: userId
     });
 
-    if(!userPlayList) throw new ApiError(500, "Something went wrong while fetching the user's playlist");
+    if (!userPlayList) throw new ApiError(500, "Something went wrong while fetching the user's playlist");
 
     res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            userPlayList,
-            "User's Playlists Fetched Successfully"
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                userPlayList,
+                "User's Playlists Fetched Successfully"
+            )
         )
-    )
 })
 
-const getPlayListById = asyncHandler( async (req, res) => {
+const getPlayListById = asyncHandler(async (req, res) => {
     const { playlistId } = req.params;
 
     console.log(playlistId)
 
-    if(!playlistId) throw new ApiError(400, "User Id is Required");
+    if (!playlistId) throw new ApiError(400, "User Id is Required");
 
     const playList = await Playlist.find({
         _id: playlistId
     });
 
-    if(!playList) throw new ApiError(500, "Something went wrong while fetching the user's playlist");
+    if (!playList) throw new ApiError(500, "Something went wrong while fetching the user's playlist");
 
     res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            playList,
-            "Playlist Fetched Successfully"
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                playList,
+                "Playlist Fetched Successfully"
+            )
         )
-    )
+})
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params;
+
+    if (!playlistId || !videoId) throw new ApiError(400, "Playlist Id and Video Id are Required");
+
+    const currentPlayList = await Playlist.findById(playlistId);
+
+    if (currentPlayList.videos.some(video => video.equals(videoId))) throw new ApiError(400, "Video Already Exist in the Playlist");
+
+    currentPlayList.videos.push(videoId);
+    await currentPlayList.save({ validateBeforeSave: false });
+
+    res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    videos: currentPlayList.videos
+                },
+                "Video Added to Playlist Successfully"
+            )
+        )
+})
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params;
+
+    if (!playlistId || !videoId) throw new ApiError(400, "Playlist Id and Video Id are Required");
+
+    const updatedPlayList = await Playlist.findByIdAndUpdate(playlistId,
+        {
+            $pull: {
+                videos: videoId
+            },
+        },
+        {
+            new: true
+        }
+    );
+
+    res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    videos: updatedPlayList.videos
+                },
+                "Video Removed from Playlist Successfully"
+            )
+        )
 })
 
 
 export {
     createPlayList,
     getUserPlayLists,
-    getPlayListById
+    getPlayListById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist
 }
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { createPlayList, getPlayListById, getUserPlayLists } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlayList, getPlayListById, getUserPlayLists, removeVideoFromPlaylist } from "../controllers/playlist.controller.js";
 
 const router = Router();
 
@@ -12,6 +12,12 @@ router.route("/").post(createPlayList);
 router.route("/p/user/:userId").get(getUserPlayLists);
 
 router.route("/p/:playlistId").get(getPlayListById);
+
+router.route("/p/add/:playlistId/:videoId").patch(addVideoToPlaylist);
+
+router.route("/p/remove/:playlistId/:videoId").patch(removeVideoFromPlaylist);
+
+
 
 
 export default router;
