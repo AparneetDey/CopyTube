dfb63d5ae3094d0357f1163434bfe01f5ad52db6
Aparneet Dey <aparnitd@gmail.com>
2025-11-01 02:54:16 +0530

    Refactor CORS configuration and enhance user authentication cookie handling
    - Updated CORS middleware to allow requests from both development and production origins, improving flexibility.
    - Added secure cookie settings for the authentication token in the user login response, enhancing security and user experience.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,20 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: process.env.PROD_ORIGIN,
-        credentials: true,
-    }
-));
+import cors from "cors";
+
+const allowedOrigins = [
+  process.env.DEV_ORIGIN, // dev frontend
+  process.env.PROD_ORIGIN, // prod frontend
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // 👈 allow cookies
+  })
+);
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -126,6 +126,13 @@ const userLogIn = asyncHandler(async (req, res) => {
 
     const loggedInUSer = await User.findById(user._id).select("-password -refreshToken");
 
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite:"none",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
     res
         .status(200)
         .cookie("accessToken", accessToken, cookieOptions)
@@ -484,14 +491,14 @@ const getUserWatchHistory = asyncHandler(async (req, res) => {
     ]);
 
     res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            user[0].watchHistory,
-            "User watch hostory Fetched Successfully"
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].watchHistory,
+                "User watch hostory Fetched Successfully"
+            )
         )
-    )
 })
 
 export {
