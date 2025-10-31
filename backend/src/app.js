1ff348a88f74046381632474d350a9bd60fbd43d
Aparneet Dey <aparnitd@gmail.com>
2025-11-01 02:42:56 +0530

    Refactor CORS configuration and streamline API route declarations
    - Simplified CORS middleware to only allow requests from the production origin, enhancing security.
    - Cleaned up API route declarations for improved readability by removing unnecessary whitespace.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,25 +4,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const allowedOrigins = [
-    process.env.DEV_ORIGIN,              // local frontend
-    process.env.PROD_ORIGIN,   // deployed frontend
-]
-
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            // Allow requests with no origin (like Postman or curl)
-            if (!origin) return callback(null, true)
-            if (allowedOrigins.includes(origin)) return callback(null, true)
-            return callback(new Error("Not allowed by CORS"))
-        },
-        credentials: true, // if using cookies or auth headers
-    })
-)
-
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cors(
+    {
+        origin: process.env.PROD_ORIGIN,
+        credentials: true,
+    }
+));
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
@@ -40,15 +29,15 @@ import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 //Router Declaration
-app.use("/api/v1/healthcheck", healthcheckRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/videos", videoRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter);
-app.use("/api/v1/comments", commentRouter);
-app.use("/api/v1/tweets", tweetRouter);
-app.use("/api/v1/likes", likeRouter);
-app.use("/api/v1/playlists", playlistRouter);
-app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck",healthcheckRouter);
+app.use("/api/v1/users",userRouter);
+app.use("/api/v1/videos",videoRouter);
+app.use("/api/v1/subscriptions",subscriptionRouter);
+app.use("/api/v1/comments",commentRouter);
+app.use("/api/v1/tweets",tweetRouter);
+app.use("/api/v1/likes",likeRouter);
+app.use("/api/v1/playlists",playlistRouter);
+app.use("/api/v1/dashboard",dashboardRouter);
 
 
 
